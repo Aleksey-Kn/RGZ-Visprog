f61aa6f17eae2fa64fcc3f4b383485b4482d98ff
Aleksey-Kn <aleksey.k2000@mail.ru>
2020-04-03 23:47:06 +0700

    Realised save table in text file

--- src/Printer.java
+++ src/Printer.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class Printer extends JFrame{
@@ -22,9 +25,14 @@ public class Printer extends JFrame{
                 if (pg != null){
                     print(pg);
                     pg.dispose();
-                }else System.err.println("Graphics's null");
+                }else{
+                    JOptionPane.showMessageDialog(null, "Graphics's null");
+                }
                 pj.end();
-            }else System.err.println("Принтер отсутствует или не подключен");
+            }else {
+                JOptionPane.showMessageDialog(null, "Принтер отсутствует или не подключен");
+            }
+            dispose();
         });
         add(printer);
 
@@ -34,7 +42,35 @@ public class Printer extends JFrame{
         JTextField textField = new JTextField();
         add(textField);
 
-        saver.addActionListener(l -> {});
+        saver.addActionListener(l -> {
+            int ColC = table.getColumnCount(); //Определяем кол-во столбцов
+            int ItemC = table.getRowCount();  //и элементов (строк)
+            StringBuilder sb = new StringBuilder();
+            FileWriter fw = null;
+            try {
+                fw = new FileWriter(new File(textField.getText()));
+            }catch (IOException e1) {
+                JOptionPane.showMessageDialog(null, e1.getMessage());
+            }
+            for(int i = 0; i < table.getColumnCount(); i++){
+                sb.append(table.getColumnName(i) + "\t");
+            }
+            sb.append("\r\n");
+            for (int i = 0; i < ItemC; i++) { //проходим все строки
+                for (int j = 0; j < ColC; j++) { //собираем одну строку из множества столбцов
+                    sb.append(table.getValueAt(i, j));
+                    if (j < ColC - 1) sb.append("\t\t");
+                    if (j == ColC - 1) sb.append("\r\n");
+                }
+            }
+            try { //Пытаемся писать в файл
+                fw.write(sb.toString()); //записывем собранную строку в файл
+                fw.close();
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
+            dispose();
+        });
 
         setVisible(true);
     }
@@ -42,6 +78,7 @@ public class Printer extends JFrame{
     public void paint(Graphics g){
         g.setFont(new Font("Serif", Font.PLAIN, 12));
         g.setColor(Color.black);
+
         StringBuilder workingString = new StringBuilder();
         int y = 40;
 
@@ -49,6 +86,14 @@ public class Printer extends JFrame{
             workingString.append(table.getColumnName(i) + "\t");
         }
         g.drawString(workingString.toString(), 30, y);
-        y += 15;
+        y += 20;
+
+        for(int i = 0; i < table.getRowCount(); i++, y += 15){
+            workingString = new StringBuilder();
+            for(int j = 0; j < table.getColumnCount(); j++){
+                workingString.append(table.getValueAt(i, j) + "\t");
+            }
+            g.drawString(workingString.toString(), 30, y);
+        }
     }
 }
